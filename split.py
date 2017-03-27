8c52667de149d2aba36d525a4997e4ebf857d05b
Douglas Horemans <douglashoremans@Douglass-MacBook-Pro.local>
2017-03-27 14:01:35 +0200

    adding splitting

---
+++ split.py
@@ -0,0 +1,19 @@
+import sys
+
+def main(argv):
+    ctr = 0
+    f = open(argv[0], "r")
+    o = open(argv[1] + str(ctr), "w")
+    
+    ctrLine = 0
+    for line in f:
+        ctrLine += 1
+        o.writelines([line])
+        if(ctrLine == 2000):
+            o.close()
+            ctr += 1
+            o = open(argv[1] + str(ctr), "w")
+            ctrLine = 0
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
