79c15e942f2bbc68d52fad546cd9aa6361f420ef
yannickv <yannick-verschueren1@hotmail.com>
2017-05-08 15:54:18 +0200

    iets

--- src/visualisation.js
+++ src/visualisation.js
@@ -165,3 +165,54 @@ function hideDetails(disaster,d) {
   disaster.select("circle").transition().attr("r",10)
   disaster.select("text").remove()
 }
+
+
+var data = d3.range(1000).map(d3.randomBates(10));
+
+var formatCount = d3.format(",.0f");
+
+var histogram = d3.select("#histogram")
+                  .append("svg")
+                  .attr("id", "graph")
+                  .attr("height", height + margin.top + margin.bottom)
+                  .attr("width", width + margin.left + margin.right);
+console.log(d3.select("#histogram"))       
+
+var x = d3.scaleLinear()
+    .rangeRound([0, width]);
+
+var bins = d3.histogram()
+    .domain(x.domain())
+    .thresholds(x.ticks(20))
+    (data);
+
+var y = d3.scaleLinear()
+    .domain([0, d3.max(bins, function(d) { return d.length; })])
+    .range([height, 0]);
+
+var bar = histogram.selectAll(".bar")
+  .data(bins)
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+bar.append("rect")
+    .attr("x", 1)
+    .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+    .attr("height", function(d) { return height - y(d.length); });
+
+bar.append("text")
+    .attr("dy", ".75em")
+    .attr("y", 6)
+    .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatCount(d.length); });
+
+histogram.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+    
+function addToPinboard(params) {
+  
+}
