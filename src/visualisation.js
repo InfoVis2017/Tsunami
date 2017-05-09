fb6680ca83d00e6d5692403e796eb4a3972807ee
yannickv <yannick-verschueren1@hotmail.com>
2017-05-09 15:36:55 +0200

    added working barchart

--- src/visualisation.js
+++ src/visualisation.js
@@ -1,7 +1,7 @@
 /* setup the dimensions */
 var margin = { top: 50, bottom: 50, left: 50, right: 50 },
     width = (0.75 * window.innerWidth) - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
 
 /* setup the projection and path generator */
 var projection = d3.geoMercator()
@@ -69,6 +69,7 @@ function registerData(name,classname,sources) {
                         })
                         .on("mouseover", function(d) {showDetails(d3.select(this),d)})
                         .on("mouseout", function(d) {hideDetails(d3.select(this),d)})
+                        .on("click", function(d) {addToPinboard(d3.select(this),d)})
 
       group.append("circle")
               .attr("class",classname)
@@ -189,53 +190,90 @@ function hideDetails(disaster,d) {
   disaster.selectAll("g").remove()
 }
 
+var chartLocation = d3.select("#chart")
+                      .append("svg")
+                      .attr("id", "graph")
+                      .attr("height", height + margin.top + margin.bottom)
+                      .attr("width", width + margin.left + margin.right);   
 
-var data = d3.range(1000).map(d3.randomBates(10));
+var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
+    y = d3.scaleLinear().rangeRound([height, 0]);
 
-var formatCount = d3.format(",.0f");
-
-var histogram = d3.select("#histogram")
-                  .append("svg")
-                  .attr("id", "graph")
-                  .attr("height", height + margin.top + margin.bottom)
-                  .attr("width", width + margin.left + margin.right);
-console.log(d3.select("#histogram"))       
-
-var x = d3.scaleLinear()
-    .rangeRound([0, width]);
-
-var bins = d3.histogram()
-    .domain(x.domain())
-    .thresholds(x.ticks(20))
-    (data);
-
-var y = d3.scaleLinear()
-    .domain([0, d3.max(bins, function(d) { return d.length; })])
-    .range([height, 0]);
-
-var bar = histogram.selectAll(".bar")
-  .data(bins)
-  .enter().append("g")
-    .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-
-bar.append("rect")
-    .attr("x", 1)
-    .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-    .attr("height", function(d) { return height - y(d.length); });
-
-bar.append("text")
-    .attr("dy", ".75em")
-    .attr("y", 6)
-    .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatCount(d.length); });
-
-histogram.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+var chart = chartLocation.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-function addToPinboard(params) {
+var ChartData = []//[{id: 0,y: 0},{id: 1,y: 1000},{id: 2,y: 1000},{id:3 ,y: 2000},{id: 4,y:3000}]
+
+//x.domain(d3.range(0,ChartData.length));
+//y.domain([0, d3.max(ChartData, function(d) { return d.y })]);
+
+chart.append("g")
+     // .attr("id","xaxis")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+      
+chart.append("g")
+      .attr("id","yaxis")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10))
+     .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Deaths")
+      .attr("fill","black");
+
+function reDrawChart() {
+  x.domain(d3.range(0,ChartData.length));
+  y.domain([0, d3.max(ChartData, function(d) { return d.y })]);
   
+//  d3.select("#xaxis").call(d3.axisBottom(x).ticks(ChartData.length))
+  d3.select("#yaxis").call(d3.axisLeft(y).ticks(10))
+  
+ var bars = chart.selectAll(".bar").data(ChartData)
+ 
+ //Remove  
+ bars.exit().remove();
+  
+ //Update
+ bars.transition()
+              .attr("class", "bar")
+              .attr("x", function(d,i) { return x(i); })
+              .attr("y", function(d) { return y(d.y);})
+              .attr("width", function(d,i) { return x.bandwidth(i);})
+              .attr("height", function(d) { return height - y(d.y);});
+    
+  //Add  
+  bars.enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d,i) { return x(i); })
+      .attr("y", function(d) { return y(d.y);})
+      .attr("width", function(d,i) { return x.bandwidth(i);})
+      .attr("height", function(d) { return height - y(d.y);})
+      .on("click",function(d) {removeFromPinboard(d)})
+      .on("mouseover",function(d) {d.circle.select("circle")
+                                        .transition()
+                                        .attr("r",50);})
+      .on("mouseout",function(d) {d.circle.select("circle")
+                                        .transition()
+                                        .attr("r",10);});
+ 
+   
+};
+
+var globalCounter = 0;
+
+function addToPinboard(groupElement,data) {
+  var newbar = {id: globalCounter, y: Math.floor(Math.random() * 3000),circle: groupElement};
+  globalCounter = globalCounter + 1;
+  ChartData.push(newbar)
+  reDrawChart();
+};
+
+function removeFromPinboard(data) {
+  ChartData = ChartData.filter(function(event) {return event.id !== data.id;})
+  reDrawChart();
 }
+reDrawChart();
