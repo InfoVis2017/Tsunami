12a6847e40f27b4360d8f163beba8008267ebeae
spartam <kwpardon@vub.ac.be>
2017-05-11 12:04:24 +0200

    autoslide
    minor bug fix

--- src/autoslide.js
+++ src/autoslide.js
@@ -3,6 +3,6 @@
       document.getElementsByTagName('input')[0].value = start;
       setYear(start);
       document.getElementsByTagName('output')[0].innerHTML = start;
-      window.setTimeout(function() {autoslide(start+1, end)}, interval);
+      window.setTimeout(function() {autoslide(start+1, end, interval)}, interval);
     }
   }
