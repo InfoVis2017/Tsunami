f5890b905d05e4256530b46e7ba5ad23bb35105f
Douglas Horemans <douglashoremans@Douglass-MacBook-Pro.local>
2017-05-17 13:30:36 +0200

    still

--- src/autoslide.js
+++ src/autoslide.js
@@ -1,10 +1,11 @@
 //value of the year slider
 var timeSlidervalue = {};
 
+//scrolling flag
 var scrolling = false;
 
-function togglescrolling(){
-  scrolling = (! scrolling);
+function togglescrolling() {
+  scrolling = (!scrolling);
   if (scrolling) {
     document.getElementById("scrolltoggle").classList.add("active");
     startautoslide();
@@ -16,22 +17,26 @@ function togglescrolling(){
   return false;
 }
 
-function startautoslide(){
- end   = 2017;
- scrolling = true;
- autoslide(end);
- return false;
+//start auto scrolling until the end is reached
+function startautoslide() {
+  end = 2017;
+  scrolling = true;
+  autoslide(end);
+  return false;
 }
 
-function autoslide(end, interval = 1000){
+//autoslide until the end
+function autoslide(end, interval = 1000) {
   var start = parseInt(document.getElementsByTagName('input')[0].value) + 1;
   if (scrolling) {
-    if (start <= end){
+    if (start <= end) {
       document.getElementsByTagName('input')[0].value = start;
+      //change the current year
       setYear(start);
-      window.setTimeout(function() {autoslide(end, interval);}, interval);
-    }
-    else {
+      window.setTimeout(function() {
+        autoslide(end, interval);
+      }, interval);
+    } else {
       togglescrolling();
     }
   }
